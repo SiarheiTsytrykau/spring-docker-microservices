619606de937b8b71e77dc2b692299997e3bda854
Siarhei Tsytrykau <siarhei_tsytrykau@epam.com>
2020-04-30 15:17:12 +0300

    Move the build process from build.sh to docker-compose.yml. Build image called server using Dockerfile under ./server dir

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd server
 docker container rm server
 docker image rm server
 ./mvnw clean package -DskipTests=true
-docker image build -t server .
+#docker image build -t server .
 cd ..
 
 echo "Building the Client service"
@@ -13,5 +13,5 @@ cd client
 docker container rm client
 docker image rm client
 ./mvnw clean package -DskipTests=true
-docker image build -t client .
+#docker image build -t client .
 cd ..
